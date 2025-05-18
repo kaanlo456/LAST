e4549154ef4abf048820181209db321e9977c1bb
kaanlo456 <kaanrifatozler@gmail.com>
2025-05-18 18:30:11 +0300

    final

--- bot.py
+++ bot.py
@@ -16,139 +16,140 @@ async def on_ready():
 
 @bot.command
 async def film(ctx):
-    await ctx.send(get_random_recommendation())
+    await ctx.send(get_random_recommendation(media_type="film"))
 
 @bot.command
 async def fbilim(ctx):
-    await ctx.send(get_random_recommendation(media_type="film"))
+    await ctx.send(get_random_recommendation(media_type="film", category="bilim kurgu"))
 
 @bot.command
 async def fkomedi(ctx):
-    await ctx.send(get_random_recommendation(media_type="film"))
+    await ctx.send(get_random_recommendation(media_type="film", category="komedi"))
 
 @bot.command
 async def fsuc(ctx):
-    await ctx.send(get_random_recommendation(media_type="film"))
+    await ctx.send(get_random_recommendation(media_type="film", category="suç"))
 
 @bot.command
 async def ftarih(ctx):
-    await ctx.send(get_random_recommendation(media_type="film"))
+    await ctx.send(get_random_recommendation(media_type="film", category="tarih"))
 
 @bot.command
 async def ftarihi(ctx):
-    await ctx.send(get_random_recommendation(media_type="film"))
+    await ctx.send(get_random_recommendation(media_type="film", category="tarihi"))
 
 @bot.command
 async def fanimasyon(ctx):
-    await ctx.send(get_random_recommendation(media_type="film"))
+    await ctx.send(get_random_recommendation(media_type="film", category="animasyon"))
 
 @bot.command
 async def fgerilim(ctx):
-    await ctx.send(get_random_recommendation(media_type="film"))
+    await ctx.send(get_random_recommendation(media_type="film", category="gerilim"))
 
 @bot.command
 async def fromantik(ctx):
-    await ctx.send(get_random_recommendation(media_type="film"))
+    await ctx.send(get_random_recommendation(media_type="film", category="romantik"))
 
 @bot.command
 async def fsavas(ctx):
-    await ctx.send(get_random_recommendation(media_type="film"))
+    await ctx.send(get_random_recommendation(media_type="film", category="savaş"))
 
 @bot.command
 async def ffantastik(ctx):
-    await ctx.send(get_random_recommendation(media_type="film"))
+    await ctx.send(get_random_recommendation(media_type="film", category="fantastik"))
 
 @bot.command
 async def fbelgesel(ctx):
-    await ctx.send(get_random_recommendation(media_type="film"))
+    await ctx.send(get_random_recommendation(media_type="film", category="belgesel"))
 
 @bot.command
 async def faksiyon(ctx):
-    await ctx.send(get_random_recommendation(media_type="film"))
+    await ctx.send(get_random_recommendation(media_type="film", category="aksiyon"))
 
 @bot.command
 async def fkorku(ctx):
-    await ctx.send(get_random_recommendation(media_type="film"))
+    await ctx.send(get_random_recommendation(media_type="film", category="korku"))
 
 @bot.command
 async def fmacera(ctx):
-    await ctx.send(get_random_recommendation(media_type="film"))
+    await ctx.send(get_random_recommendation(media_type="film", category="macera"))
 
 @bot.command
 async def fgizem(ctx):
-    await ctx.send(get_random_recommendation(media_type="film"))
+    await ctx.send(get_random_recommendation(media_type="film", category="gizem"))
 
 @bot.command
 async def fdram(ctx):
-    await ctx.send(get_random_recommendation(media_type="film"))
+    await ctx.send(get_random_recommendation(media_type="film", category="dram"))
+
 
 @bot.command
 async def dizi(ctx):
-    await ctx.send(get_random_recommendation())
+    await ctx.send(get_random_recommendation(media_type="dizi"))
 
 @bot.command
 async def dbilim(ctx):
-    await ctx.send(get_random_recommendation(media_type="film"))
+    await ctx.send(get_random_recommendation(media_type="dizi", category="bilim"))
 
 @bot.command
 async def dkomedi(ctx):
-    await ctx.send(get_random_recommendation(media_type="film"))
+    await ctx.send(get_random_recommendation(media_type="dizi", category="komedi"))
 
 @bot.command
 async def dsuc(ctx):
-    await ctx.send(get_random_recommendation(media_type="film"))
+    await ctx.send(get_random_recommendation(media_type="dizi", category="suç"))
 
 @bot.command
 async def dtarih(ctx):
-    await ctx.send(get_random_recommendation(media_type="film"))
+    await ctx.send(get_random_recommendation(media_type="dizi", category="tarih"))
 
 @bot.command
 async def dtarihi(ctx):
-    await ctx.send(get_random_recommendation(media_type="film"))
+    await ctx.send(get_random_recommendation(media_type="dizi", category="tarihi"))
 
 @bot.command
 async def danimasyon(ctx):
-    await ctx.send(get_random_recommendation(media_type="film"))
+    await ctx.send(get_random_recommendation(media_type="dizi", category="animasyon"))
 
 @bot.command
 async def dgerilim(ctx):
-    await ctx.send(get_random_recommendation(media_type="film"))
+    await ctx.send(get_random_recommendation(media_type="dizi", category="gerilim"))
 
 @bot.command
 async def dromantik(ctx):
-    await ctx.send(get_random_recommendation(media_type="film"))
+    await ctx.send(get_random_recommendation(media_type="dizi", category="romantik"))
 
 @bot.command
 async def dsavas(ctx):
-    await ctx.send(get_random_recommendation(media_type="film"))
+    await ctx.send(get_random_recommendation(media_type="dizi", category="savaş"))
 
 @bot.command
 async def dfantastik(ctx):
-    await ctx.send(get_random_recommendation(media_type="film"))
+    await ctx.send(get_random_recommendation(media_type="dizi", category="fantastik"))
 
 @bot.command
 async def dbelgesel(ctx):
-    await ctx.send(get_random_recommendation(media_type="film"))
+    await ctx.send(get_random_recommendation(media_type="dizi", category="belgesel"))
 
 @bot.command
 async def daksiyon(ctx):
-    await ctx.send(get_random_recommendation(media_type="film"))
+    await ctx.send(get_random_recommendation(media_type="dizi", category="aksiyon"))
 
 @bot.command
 async def dkorku(ctx):
-    await ctx.send(get_random_recommendation(media_type="film"))
+    await ctx.send(get_random_recommendation(media_type="dizi", category="korku"))
 
 @bot.command
 async def dmacera(ctx):
-    await ctx.send(get_random_recommendation(media_type="film"))
+    await ctx.send(get_random_recommendation(media_type="dizi", category="macera"))
 
 @bot.command
 async def dgizem(ctx):
-    await ctx.send(get_random_recommendation(media_type="film"))
+    await ctx.send(get_random_recommendation(media_type="dizi", category="gizem"))
 
 @bot.command
 async def ddram(ctx):
-    await ctx.send(get_random_recommendation(media_type="film"))
+    await ctx.send(get_random_recommendation(media_type="dizi", category="dram"))
 
 @bot.command
 async def devs(ctx):
